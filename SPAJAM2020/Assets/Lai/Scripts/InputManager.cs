8dcffa9b9aa0d1ff78f532a3d3b8ad3b6164ccc8
khslai <44391813+khslai@users.noreply.github.com>
2020-10-03 16:55:27 +0900

    Note.csを実装した

--- SPAJAM2020/Assets/Lai/Scripts/InputManager.cs
+++ SPAJAM2020/Assets/Lai/Scripts/InputManager.cs
@@ -6,41 +6,49 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    [SerializeField] Note note = null;
+
     void Update()
     {
-        //var touch = Input.GetTouch(0);
-        //switch (touch.phase)
-        //{
-        //    // 画面に指が触れた時に行いたい処理をここに書く
-        //    case TouchPhase.Began:
-
-        //        ButtonDown();
-        //        break;
-
-        //    // 画面上で指が動いたときに行いたい処理をここに書く
-        //    case TouchPhase.Moved:
-        //        break;
-        //    // 指が画面に触れているが動いてはいない時に行いたい処理をここに書く
-        //    case TouchPhase.Stationary:
-        //        break;
-        //    // 画面から指が離れた時に行いたい処理をここに書く
-        //    case TouchPhase.Ended:
-        //        break;
-        //    // システムがタッチの追跡をキャンセルした時に行いたい処理をここに書く
-        //    case TouchPhase.Canceled:
-        //        break;
-        //}
-
 #if UNITY_EDITOR
         if (Input.GetMouseButtonDown(0))
         {
-            ButtonDown();
+            ButtonDown(0f);
+        }
+#else
+        var touch = Input.GetTouch(0);
+        switch (touch.phase)
+        {
+            // 画面に指が触れた時に行いたい処理をここに書く
+            case TouchPhase.Began:
+
+                ButtonDown();
+                break;
+
+            // 画面上で指が動いたときに行いたい処理をここに書く
+            case TouchPhase.Moved:
+                break;
+            // 指が画面に触れているが動いてはいない時に行いたい処理をここに書く
+            case TouchPhase.Stationary:
+                break;
+            // 画面から指が離れた時に行いたい処理をここに書く
+            case TouchPhase.Ended:
+                break;
+            // システムがタッチの追跡をキャンセルした時に行いたい処理をここに書く
+            case TouchPhase.Canceled:
+                break;
         }
 #endif
     }
 
-    private void ButtonDown()
+    private void ButtonDown(float spawnTime)
     {
-        Debug.Log("Touch");
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = 0f;
+        //Debug.Log("worldPos" + worldPos.x + " " + worldPos.y + " " + worldPos.z);
+
+        Note temp = Instantiate(note);
+        temp.transform.position = worldPos;
+        temp.SpawnTime = spawnTime;
     }
 }
