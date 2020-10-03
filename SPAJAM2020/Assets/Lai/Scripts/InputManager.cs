de17ae893ae18c55d889555484a48ef0922e7f07
khslai <44391813+khslai@users.noreply.github.com>
2020-10-03 16:01:06 +0900

    InputManagerの実装

---
+++ SPAJAM2020/Assets/Lai/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// InputManager : 入力マネージャー
+
+public class InputManager : MonoBehaviour
+{
+    void Update()
+    {
+        //var touch = Input.GetTouch(0);
+        //switch (touch.phase)
+        //{
+        //    // 画面に指が触れた時に行いたい処理をここに書く
+        //    case TouchPhase.Began:
+
+        //        ButtonDown();
+        //        break;
+
+        //    // 画面上で指が動いたときに行いたい処理をここに書く
+        //    case TouchPhase.Moved:
+        //        break;
+        //    // 指が画面に触れているが動いてはいない時に行いたい処理をここに書く
+        //    case TouchPhase.Stationary:
+        //        break;
+        //    // 画面から指が離れた時に行いたい処理をここに書く
+        //    case TouchPhase.Ended:
+        //        break;
+        //    // システムがタッチの追跡をキャンセルした時に行いたい処理をここに書く
+        //    case TouchPhase.Canceled:
+        //        break;
+        //}
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            ButtonDown();
+        }
+#endif
+    }
+
+    private void ButtonDown()
+    {
+        Debug.Log("Touch");
+    }
+}
