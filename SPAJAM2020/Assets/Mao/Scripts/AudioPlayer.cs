18a169616b3860921031068aef315e7408f7f916
mdelta137 <moderndelta137@gmail.com>
2020-10-04 02:19:06 +0900

    Import ripple FX & add SE
    ・Installed Universal Render Pipeline package
    ・Add bloom post process to master game scene.
    ・Imported ripple FX
     　・Add ripple FX to notes
    ・Added script to play audio
    　・Add BGM to master game scene
    　・Add SE to notes

---
+++ SPAJAM2020/Assets/Mao/Scripts/AudioPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip[] clips;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = this.GetComponent<AudioSource>();
+        ShuffleBGM();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ShuffleBGM()
+    {
+        source.clip = clips[Random.Range(0,clips.Length)];
+        source.Play();
+    }
+}
